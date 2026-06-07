0f69e92545e096282763a27ad376cbb06ce82190
Luis Graciolli <luisfelipegraciolli@gmail.com>
2026-06-07 13:57:57 -0300

    refactor: Move Top Bar UI element to HorrorScreen.kt

--- app/src/main/java/com/example/thirtydaysofhorror/HorrorScreen.kt
+++ app/src/main/java/com/example/thirtydaysofhorror/HorrorScreen.kt
@@ -27,7 +27,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExpandLess
@@ -36,7 +35,6 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -47,7 +45,6 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import com.example.thirtydaysofhorror.model.HorrorMedia
-import com.example.thirtydaysofhorror.ui.theme.ThirtyDaysOfHorrorTheme
 
 
 @OptIn(ExperimentalMaterial3Api::class)
